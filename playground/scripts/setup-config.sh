ca99cd519590408825c070c61e7186be1d7144aa
Sam Kim <samkim@gmail.com>
2024-03-20 14:58:17 -0700

    Vercel build info

--- playground/scripts/setup-config.sh
+++ playground/scripts/setup-config.sh
@@ -5,7 +5,9 @@ echo "setup-config directory: $DIR"
 cd "$DIR/../contrib"
 
 env_file="../build/config-env.js"
+echo "contrib directory: $(pwd)"
+echo "contrib: $(ls)"
 
-. generate-config-env.sh
+. ./generate-config-env.sh
 validate_env "$env_file"
 generate_env "$env_file"
