ea04ff458f382dcbf7ca7079d38f784eb9cb8ace
Sam Kim <sam@authzed.com>
2024-10-09 20:00:56 -0400

    Add support to configure content security policy header (#19)
    * Add support to configure content security policy header
    
    * Add default domains for vercel config

--- playground/contrib/generate-config-env.sh
+++ playground/contrib/generate-config-env.sh
@@ -28,7 +28,7 @@ EOF
 generate_nginx() {
     conf_template=$1
     nginx_conf=$2
-    envsubst '$PLAYGROUND_DEVELOPER_API_DOWNLOAD_ENDPOINT' < "$conf_template" > "$nginx_conf"
+    envsubst '$PLAYGROUND_DEVELOPER_API_DOWNLOAD_ENDPOINT,$HEADER_CONTENT_SECURITY_POLICY' < "$conf_template" > "$nginx_conf"
     echo "${nginx_conf}:"
     cat "$nginx_conf"
 }
