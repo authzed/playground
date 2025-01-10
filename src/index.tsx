1e1dc140107f228b09501ca89939a66ecca9d9d0
Tanner Stirrat <tstirrat@gmail.com>
2025-01-10 15:46:21 -0700

    Fix fontsource and remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import "fontsource-roboto";
+import "@fontsource/roboto";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,8 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
