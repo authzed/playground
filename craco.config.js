5c889f6197ba6fd69ce78f3bea4ee5eb582d5238
Tanner Stirrat <tstirrat@gmail.com>
2025-01-10 14:50:49 -0700

    Get rid of unnecessary module resolutions:

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,8 @@
 // craco.config.js
 const CracoEsbuildPlugin = require('craco-esbuild');
-const path = require('path');
 
 const HEADER_CONTENT_SECURITY_POLICY =
   process.env.HEADER_CONTENT_SECURITY_POLICY || '';
-
-const resolvePackage = (relativePath) => {
-  return path.resolve(__dirname, relativePath);
-};
 
 module.exports = {
   webpack: {
@@ -28,10 +23,6 @@ module.exports = {
     {
       plugin: CracoEsbuildPlugin,
       options: {
-        includePaths: [
-          resolvePackage('../playground-ui'),
-          resolvePackage('../spicedb-common'),
-        ], // Optional. If you want to include components which are not in src folder
         enableSvgr: true, // Optional.
         svgrOptions: {
           removeViewBox: false,
