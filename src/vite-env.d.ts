19e6956d7837e6d912a3d1319def8d863e1cb6d3
Tanner Stirrat <tstirrat@gmail.com>
2025-01-13 11:21:19 -0700

    Add typescript types for environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_OIDC_URL_PREFIX?: string;
+  readonly VITE_AUTHENTICATION_ENGINE?: string;
+
+  readonly VITE_AUTHZED_DEVELOPER_GATEWAY_ENDPOINT?: string | undefined | null;
+  readonly VITE_GOOGLE_ANALYTICS_MEASUREMENT_ID?: string;
+
+  readonly VITE_DISCORD_CHANNEL_ID?: string;
+  readonly VITE_DISCORD_INVITE_URL?: string;
+  readonly VITE_DISCORD_SERVER_ID?: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
