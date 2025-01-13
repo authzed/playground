f40dfab963d20ee9b2164d1882d4fa861dce01dd
Tanner Stirrat <tstirrat@gmail.com>
2025-01-13 11:25:21 -0700

    Fix import, add TODO

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import "@fontsource/roboto";
+import "@fontsource/roboto/index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// TODO: use React.createRoot
 ReactDOM.render(
   <React.StrictMode>
     <App withRouter={BrowserRouter} />
