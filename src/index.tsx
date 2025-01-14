04340205adffe33efcdd15f1ea1fe9d0f72524a0
Tanner Stirrat <tstirrat@gmail.com>
2025-01-14 11:07:52 -0700

    Use createRoot instead of render)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import "@fontsource/roboto/index.css";
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-// TODO: use React.createRoot
-ReactDOM.render(
+const container = document.getElementById("root")
+if (container) {
+createRoot(
+    container
+).render(
   <React.StrictMode>
     <App withRouter={BrowserRouter} />
   </React.StrictMode>,
-  document.getElementById("root")
 );
+}
