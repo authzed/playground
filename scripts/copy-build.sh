c8d43d37de9f19d2120de3734df835dff00114af
Tanner Stirrat <tstirrat@gmail.com>
2025-01-14 10:50:52 -0700

    Fix build script

--- scripts/copy-build.sh
+++ scripts/copy-build.sh
@@ -2,7 +2,7 @@
 # TODO: do this with vite config or moving the folders
 # instead of a script
 
-cp wasm/*.wasm  dist/static
 mkdir -p dist/static/schemas
+cp wasm/*.wasm  dist/static
 cp -R examples/schemas/* dist/static/schemas
-ls build/static/schemas > dist/static/schemas/_all
+ls dist/static/schemas > dist/static/schemas/_all
