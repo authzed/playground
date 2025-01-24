774af4798845a387fb2b58df8d6e19154b50d723
Tanner Stirrat <tstirrat@gmail.com>
2025-01-24 14:43:50 -0700

    Add dts file for visjs

---
+++ src/playground-ui/visjs-network.d.ts
@@ -0,0 +1,6 @@
+declare module "visjs-network" {
+  // TODO: go reverse-engineer these types
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const data: any;
+  export default data;
+}
