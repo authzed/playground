e78d539d032474dc836efc50bf9138e0c16945ab
Tanner Stirrat <tstirrat@gmail.com>
2025-04-30 15:51:57 -0600

    Use import.meta.glob to include examples

--- scripts/copy-build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# TODO: do this with vite config or moving the folders
-# instead of a script
-
-mkdir -p build/static/schemas
-cp -R examples/schemas/* build/static/schemas
-ls build/static/schemas > build/static/schemas/_all
--- src/spicedb-common/examples.ts
+++ src/spicedb-common/examples.ts
@@ -1,4 +1,5 @@
 import yaml from "yaml";
+import { sortBy, zip } from "es-toolkit";
 import { ParsedValidation } from "./validationfileformat";
 export interface Example {
   id: string;
@@ -8,37 +9,54 @@ export interface Example {
   data: ParsedValidation;
 }
 
-async function get(path: string): Promise<string> {
-  const result = await fetch(path);
-  return await result.text();
-}
+const readmesImports = import.meta.glob("/examples/schemas/*/README.md", {
+  // Get just the raw text
+  query: "?raw",
+});
+const schemasImports = import.meta.glob(
+  "/examples/schemas/*/schema-and-data.yaml",
+  {
+    // Get just the raw text
+    query: "?raw",
+  },
+);
 
-const prefix = `/static/schemas`;
+const isTextImport = (input: unknown): input is { default: string } => {
+  return typeof input === "object" && input !== null && "default" in input;
+};
+
+const sortAndRealizeImports = async (
+  imports: Record<string, () => Promise<unknown>>,
+) => {
+  const realizedImports = await Promise.all(
+    sortBy(Object.entries(imports), [0]).map(([, importThunk]) =>
+      importThunk(),
+    ),
+  );
+  return realizedImports
+    .filter(isTextImport)
+    .map(({ default: defaultExport }) => defaultExport);
+};
 
 /**
  * LoadExamples loads the examples defined statically and compiled in.
  */
 export async function LoadExamples(): Promise<Example[]> {
-  // NOTE: the format of the file tree here is defined by the Dockerfile for playground-ui/playground
-  // and matches the tree structure found in https://github.com/authzed/examples/tree/main/schemas.
-  // `_all` is added by the Dockerfile at build time.
-  const exampleNames = (await get(`${prefix}/_all`))
-    .split("\n")
-    .filter((n) => !!n && n !== "_all");
-  const examples: Example[] = [];
-  for (const n of exampleNames) {
-    const documentation = await get(`${prefix}/${n}/README.md`);
-    const data = await get(`${prefix}/${n}/schema-and-data.yaml`);
-    const title = documentation.split("\n")[0].trim().substring(1).trim(); // Strip the # for the markdown header
-    const subtitle = documentation.split("\n")[2].trim();
-
-    examples.push({
-      id: n,
-      title: title,
-      subtitle: subtitle,
-      documentation: documentation,
-      data: yaml.parse(data) as ParsedValidation,
-    });
-  }
-  return examples;
+  const names = Object.entries(readmesImports).map(([path]) => path);
+  names.sort();
+  const sortedReadmes = await sortAndRealizeImports(readmesImports);
+  const sortedSchemas = await sortAndRealizeImports(schemasImports);
+  const zippedImports = zip(names, sortedReadmes, sortedSchemas);
+  return zippedImports.map(([name, readme, schema]) => {
+    const lines = readme.split("\n");
+    const title = lines[0].trim().substring(1).trim(); // Strip the # for the markdown header
+    const subtitle = lines[2].trim();
+    return {
+      id: name,
+      title,
+      subtitle,
+      documentation: readme,
+      data: yaml.parse(schema),
+    };
+  });
 }
