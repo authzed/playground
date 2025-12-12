f8fa7040a1e10cb65f6cf88e624b09846bcb3449
Maria Ines Parnisari <maria.ines.parnisari@authzed.com>
2025-12-12 15:34:33 -0800

    fix: loading of examples (#92)
    * fix: loading of examples
    
    * fix: test in GHA

--- src/spicedb-common/examples.ts
+++ src/spicedb-common/examples.ts
@@ -1,10 +1,8 @@
 import yaml from "yaml";
-import { sortBy, zip } from "es-toolkit";
 import { ParsedValidation } from "./validationfileformat";
 export interface Example {
   id: string;
   title: string;
-  documentation: string;
   subtitle: string;
   data: ParsedValidation;
 }
@@ -12,12 +10,14 @@ export interface Example {
 const readmesImports = import.meta.glob("/examples/schemas/*/README.md", {
   // Get just the raw text
   query: "?raw",
+  eager: true,
 });
 const schemasImports = import.meta.glob(
   "/examples/schemas/*/schema-and-data.yaml",
   {
     // Get just the raw text
     query: "?raw",
+    eager: true,
   },
 );
 
@@ -25,38 +25,47 @@ const isTextImport = (input: unknown): input is { default: string } => {
   return typeof input === "object" && input !== null && "default" in input;
 };
 
-const sortAndRealizeImports = async (
-  imports: Record<string, () => Promise<unknown>>,
-) => {
-  const realizedImports = await Promise.all(
-    sortBy(Object.entries(imports), [0]).map(([, importThunk]) =>
-      importThunk(),
-    ),
-  );
-  return realizedImports
-    .filter(isTextImport)
-    .map(({ default: defaultExport }) => defaultExport);
-};
-
 /**
- * LoadExamples loads the examples defined statically and compiled in.
+ * LoadExamples loads the examples defined statically and compiled in the /examples repo.
+ * If one example is lacking the schema file, we don't include it.
  */
-export async function LoadExamples(): Promise<Example[]> {
-  const names = Object.entries(readmesImports).map(([path]) => path);
-  names.sort();
-  const sortedReadmes = await sortAndRealizeImports(readmesImports);
-  const sortedSchemas = await sortAndRealizeImports(schemasImports);
-  const zippedImports = zip(names, sortedReadmes, sortedSchemas);
-  return zippedImports.map(([name, readme, schema]) => {
-    const lines = readme.split("\n");
-    const title = lines[0].trim().substring(1).trim(); // Strip the # for the markdown header
-    const subtitle = lines[2].trim();
-    return {
-      id: name,
-      title,
-      subtitle,
-      documentation: readme,
-      data: yaml.parse(schema),
-    };
-  });
+export function LoadExamples(): Example[] {
+  return Object.keys(readmesImports)
+    .map((path) => {
+      // Grab the id
+      const match = path.match(/\/examples\/schemas\/(.*)\/README\.md/);
+      return match ? match[1] : "";
+    })
+    .filter((name): name is string => {
+      if (name === "") return false;
+      // Skip examples that don't have the schema file
+      const schemaPath = `/examples/schemas/${name}/schema-and-data.yaml`;
+      return schemaPath in schemasImports;
+    })
+    .sort()
+    .map((name) => {
+      const readmeModule =
+        readmesImports[`/examples/schemas/${name}/README.md`];
+      const schemaModule =
+        schemasImports[`/examples/schemas/${name}/schema-and-data.yaml`];
+
+      const readme = isTextImport(readmeModule) ? readmeModule.default : "";
+      const schema = isTextImport(schemaModule) ? schemaModule.default : "";
+
+      const lines = readme.split("\n");
+      const title = lines[0].trim().substring(1).trim(); // Strip the # for the markdown header
+      const subtitle = lines[2].trim();
+      let schemaRes: ParsedValidation;
+      try {
+        schemaRes = yaml.parse(schema);
+      } catch (e) {
+        throw new Error("error parsing schema for example " + name + ": " + e);
+      }
+      return {
+        id: name,
+        title: title,
+        subtitle: subtitle,
+        data: schemaRes,
+      };
+    });
 }
